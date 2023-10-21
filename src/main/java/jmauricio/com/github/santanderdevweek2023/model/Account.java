dba64e455c7ea501e5c566b8f992e4c59e5c11a0
José Maurício <mauricio.rodriguesdelima@gmail.com>
2023-10-21 12:35:26 -0300

    feat(classes): mapeamento de classes para criação das entidades.

--- src/main/java/jmauricio/com/github/santanderdevweek2023/model/Account.java
+++ src/main/java/jmauricio/com/github/santanderdevweek2023/model/Account.java
@@ -1,13 +1,24 @@
 package jmauricio.com.github.santanderdevweek2023.model;
 
+import jakarta.persistence.*;
+
 import java.math.BigDecimal;
 
+@Entity(name = "tb_account")
 public class Account {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(unique = true)
     private String number;
     private String agency;
+
+    @Column(scale = 13, precision = 2)
     private BigDecimal balance;
+
+    @Column(name = "additional_limit", scale = 13, precision = 2)
     private BigDecimal limit;
 
     public Long getId() {
--- src/main/java/jmauricio/com/github/santanderdevweek2023/model/News.java
+++ src/main/java/jmauricio/com/github/santanderdevweek2023/model/News.java
@@ -1,32 +1,8 @@
 package jmauricio.com.github.santanderdevweek2023.model;
 
-public class News {
+import jakarta.persistence.Entity;
+import jmauricio.com.github.santanderdevweek2023.BaseItem;
 
-    private Long id;
-    private String icon;
-    private String description;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getIcon() {
-        return icon;
-    }
-
-    public void setIcon(String icon) {
-        this.icon = icon;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
+@Entity(name = "tb_news")
+public class News extends BaseItem {
 }
